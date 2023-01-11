6ff22a63edfde48c00195b0ccb613c17156ea222
helen <helen@LAPTOP-QN3F12HT.mynetworksettings.com>
2023-01-10 21:02:17 -0500

    new commit

---
+++ src/Instructions.java
@@ -0,0 +1,17 @@
+
+public class Instructions {
+	
+ // replicate the same steps(3)
+	//create new java project
+	//delete the project 
+	//make sure to click on the check box
+	
+	//creating local repository
+	//right click on the project name
+	//move to team
+	//click on the share project
+	//click on the sahre boc to use or create new repository
+	//click on the 
+
+}
+
